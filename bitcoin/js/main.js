3084195a145c6de28a1cb4a2607c5dad8ef19a5d
dasman74 <yuryo.fujita@gmail.com>
2019-03-17 15:58:28 +0900

    bitcoin表示

--- bitcoin/js/main.js
+++ bitcoin/js/main.js
@@ -4,6 +4,13 @@ new Vue({
     bpi:null,
   },
   mounted: function(){
-    
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(function(response){
+      //console.log(response.data.bpi)
+      this.bpi=response.data.bpi
+    }.bind(this))
+    .catch(function(error){
+      console.log(error)
+    })
   }
 })
