2f0b2913b673fdabf8e1348c80f1f2624f162f30
dasman74 <yuryo.fujita@gmail.com>
2019-03-19 06:32:41 +0900

    bind

---
+++ section5_js/js/main.js
@@ -0,0 +1,33 @@
+//グローバルフィルタリング
+Vue.filter('numberFormat', function(value){
+    return value.toLocaleString()
+}),
+Vue.filter('toUSD', function(jpy){
+    return jpy / 100
+}),
+/**
+ * suffix 接尾辞
+ */
+
+Vue.filter('readMore', function(text, length, suffix){
+    return text.substring(0, length) + suffix
+
+})
+var app = new Vue({
+  el: '#app',
+  data:{
+      message:'hello vue js',
+      count:100,
+      ok:true,
+      price:298000,
+      jpyPrice:298000,
+      text:'dsfsddddddddddddddddddddddddddddddddddddddddddddddddd',
+      url:'https://www.google.com'
+  },
+  //フィルタリング
+  /*filters: {
+      numberFormat : function(value){
+          return value.toLocaleString()
+      }
+  }*/
+})
